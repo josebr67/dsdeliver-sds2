a91e04bbdb1f7604c5aaca90c1c87df5b41e4c19
jose luiz brunoni <jlbru89@gmail.com>
2021-01-06 20:35:14 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Navbar from './Navbar';
+import Home from './Home';
 function App() {
   return (
-    <div>
+    
+    <>  
      <Navbar />
-    </div>
+     <Home />
+    </> 
+    
   );
 }
 
